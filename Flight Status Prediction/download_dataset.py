c495cbf053a70983b94c4c3abf7d91095b706ead
Hasan Emin <emin.oren.139@gmail.com>
2024-11-01 18:14:04 +0300

    Change dataset download and start dataset analysis
    Done useless features and missing values sections
    Start numerical features section

--- Flight Status Prediction/download_dataset.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-import shutil
-
-def download(dataset_name, file_name=None):
-    path_to_file = kagglehub.dataset_download(dataset_name, path=file_name)
-    return shutil.move(path_to_file, "./")
